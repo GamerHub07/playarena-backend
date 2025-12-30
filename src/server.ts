bb91541495aa333531ddf72913a71cddbdd42846
aditya-shrimali <shrimaliaditya013@gmail.com>
2025-12-30 20:23:03 +0530

    token disappering bug fixed

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ const io = initializeSocket(httpServer);
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  origin:  'http://localhost:3000',
   credentials: true,
 }));
 app.use(express.json());
