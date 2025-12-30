9f269975af0edfb7bda988489ffbaaa66a5c928a
aditya-shrimali <shrimaliaditya013@gmail.com>
2025-12-30 22:40:02 +0530

    merging

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,6 @@ const io = initializeSocket(httpServer);
 
 // Middleware
 app.use(helmet());
-console.log("CORS_ORIGIN", process.env.CORS_ORIGIN || 'http://localhost:3000');
 app.use(cors({
   origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
   credentials: true,
